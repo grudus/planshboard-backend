4fe60133aa6d9414c7d6ded47e2c4799ac143608
grudus <jakgruda@gmail.com>
2020-02-08 16:15:53 +0100

    Initial commit

---
+++ backend/src/main/kotlin/com/grudus/planshboard/PlanshboardAppApplication.kt
@@ -0,0 +1,11 @@
+package com.grudus.planshboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanshboardAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlanshboardAppApplication>(*args)
+}
