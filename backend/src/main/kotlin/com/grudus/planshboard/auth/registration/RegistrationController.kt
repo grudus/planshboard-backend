04e2e8f1d9e4f398db40b9606e9e91d9fe8ffdb4
grudus <jakgruda@gmail.com>
2020-02-26 23:43:02 +0100

    Add registration tests

--- backend/src/main/kotlin/com/grudus/planshboard/auth/registration/RegistrationController.kt
+++ backend/src/main/kotlin/com/grudus/planshboard/auth/registration/RegistrationController.kt
@@ -5,10 +5,8 @@ import com.grudus.planshboard.commons.responses.IdResponse
 import com.grudus.planshboard.commons.responses.idOf
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/auth/registration")
@@ -20,6 +18,7 @@ constructor(private val userAuthenticationService: UserAuthenticationService,
     private val log = LoggerFactory.getLogger(javaClass.simpleName)
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun registerUser(@RequestBody registerUserRequest: RegisterUserRequest): IdResponse {
         registerUserValidator.validate(registerUserRequest)
 
---
+++ backend/src/test/kotlin/com/grudus/planshboard/AbstractControllerTest.kt
@@ -0,0 +1,50 @@
+package com.grudus.planshboard
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.grudus.planshboard.configuration.filters.StatelessAuthenticationFilter
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+abstract class AbstractControllerTest : SpringBasedTest() {
+
+    @Autowired
+    private lateinit var wac: WebApplicationContext
+
+    @Autowired
+    private lateinit var statelessAuthenticationFilter: StatelessAuthenticationFilter
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    private lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    fun setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+            .addFilters<DefaultMockMvcBuilder>(statelessAuthenticationFilter)
+            .build()
+    }
+
+    @AfterEach
+    fun cleanUp() {
+        SecurityContextHolder.clearContext()
+    }
+
+    protected fun postRequest(url: String, requestBody: Any): ResultActions =
+        mockMvc.perform(MockMvcRequestBuilders.post(url)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(toJson(requestBody)))
+
+    private fun toJson(o: Any): ByteArray =
+        objectMapper.writeValueAsBytes(o)
+}
+
