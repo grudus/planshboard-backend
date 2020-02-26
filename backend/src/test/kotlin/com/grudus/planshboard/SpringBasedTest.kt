2ebec5e7b3a3e3fa56dd5af6986c3c20745a4050
grudus <grudus@users.noreply.github.com>
2020-02-27 00:01:53 +0100

    All spring based tests are annotated with @SpringBootTest. Set test log level to INFO

--- backend/src/test/kotlin/com/grudus/planshboard/SpringBasedTest.kt
+++ backend/src/test/kotlin/com/grudus/planshboard/SpringBasedTest.kt
@@ -1,13 +1,12 @@
 package com.grudus.planshboard
 
-import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.Rollback
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.transaction.annotation.Transactional
 
-@ExtendWith(SpringExtension::class)
+@SpringBootTest
 @ContextConfiguration(classes = [TestContext::class])
 @WebAppConfiguration
 @Transactional
