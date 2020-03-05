1a630d46061aa61a3d9672c8e3a6c60d2fac7b50
grudus <jakgruda@gmail.com>
2020-03-05 23:46:16 +0100

    Allow absolute imports; Define responsive breakpoints

--- frontend/src/app/auth/auth-wrapper/AuthFormWrapper.tsx
+++ frontend/src/app/auth/auth-wrapper/AuthFormWrapper.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import css from "./auth-wrapper.module.scss";
 import { merge } from "../../../utils/cssUtils";
-import useTranslations from "../../locale/hooks/useTranslations";
 
 const AuthFormWrapper: React.FC = props => {
-    const { translate } = useTranslations();
-
     return (
         <div className={css.wrapper}>
-            <div className={merge(css.halfItem, css.image)}>{translate("APP_TITLE")}</div>
+            <div className={merge(css.halfItem, css.image)} />
             <div className={css.halfItem}>{props.children}</div>
         </div>
     );
