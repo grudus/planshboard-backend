2ec07377f18598df73698cdcef10dfb709c43c23
grudus <grudus@users.noreply.github.com>
2020-03-07 15:48:25 +0100

    Create login form design

--- frontend/src/app/auth/login/Login.tsx
+++ frontend/src/app/auth/login/Login.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import useTranslations from "app/locale/hooks/useTranslations";
 import AuthFormWrapper from "app/auth/auth-wrapper/AuthFormWrapper";
+import Input from "library/input/Input";
+import Button from "library/button/Button";
+import css from "./login.module.scss";
+import image from "./login-left-3.svg";
 
 const Login: React.FunctionComponent<any> = () => {
     const { translate } = useTranslations();
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+
+    const tryToLogin = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        alert(`LOGIN ${login} ${password}`);
+    };
+
     return (
-        <AuthFormWrapper>
-            <h1>{translate("AUTH.LOGIN.TITLE")}</h1>
+        <AuthFormWrapper image={image} text={translate("AUTH.LOGIN.IMAGE_TITLE")}>
+            <form onSubmit={tryToLogin} className={css.loginForm}>
+                <h1 className={css.title}>{translate("AUTH.LOGIN.TITLE")}</h1>
+
+                <Input placeholderKey="AUTH.LOGIN.INPUT_LOGIN_PLACEHOLDER" onChange={setLogin} autoFocus />
+                <Input placeholderKey="AUTH.LOGIN.INPUT_PASSWORD_PLACEHOLDER" onChange={setPassword} />
+
+                <Button textKey="AUTH.LOGIN.BUTTON" color="primary" fullWidth className={css.button} type="submit" />
+            </form>
         </AuthFormWrapper>
     );
 };
--- frontend/src/app/locale/hooks/useTranslations.tsx
+++ frontend/src/app/locale/hooks/useTranslations.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { useRedux } from "../../../store/rootReducer";
-import { getNestedTranslation } from "../../../utils/objectUtils";
+import { useRedux } from "store/rootReducer";
+import { getNestedTranslation } from "utils/objectUtils";
 
 const useTranslations = () => {
     const { translations } = useRedux(state => state.locale);
 
-    const translate = React.useCallback((key: string) => getNestedTranslation(key, translations), [translations]);
+    const translate = React.useCallback((key: string): string => key && getNestedTranslation(key, translations), [
+        translations,
+    ]);
     return { translate };
 };
 
