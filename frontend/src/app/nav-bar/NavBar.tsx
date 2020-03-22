4e2831968eeaa34145d23fa9d1cc49d642d8ef3d
grudus <grudus@users.noreply.github.com>
2020-03-22 12:57:26 +0100

    Create mobile bottom navigation bar

--- frontend/src/app/nav-bar/NavBar.tsx
+++ frontend/src/app/nav-bar/NavBar.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode } from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { appRoutes, routesWithoutNav } from "app/routing/routes";
 import useTranslations from "app/locale/hooks/useTranslations";
-import NavBarMenuList, { NavBarMenuItem } from "app/nav-bar/NavBarMenuList";
+import DesktopNavBar from "app/nav-bar/desktop/DesktopNavBar";
 import { ReactComponent as BoardGamesIcon } from "./icons/board-games.svg";
 import { ReactComponent as StatsIcon } from "./icons/stats.svg";
 import { ReactComponent as PlaysIcon } from "./icons/plays.svg";
@@ -10,6 +10,19 @@ import { ReactComponent as SettingsIcon } from "./icons/settings.svg";
 import { ReactComponent as RankingIcon } from "./icons/ranking.svg";
 import css from "./nav-bar.module.scss";
 import { useLocation } from "react-router-dom";
+import { useSize } from "app/shared/hooks/useSize";
+import MobileNavBar from "app/nav-bar/mobile/MobileNavBar";
+
+export interface NavBarMenuItem {
+    path: string;
+    label: string;
+    icon: ReactElement;
+}
+
+export interface NavBarMenuListProps {
+    items: NavBarMenuItem[];
+    currentPath: string;
+}
 
 function menu(translate: Function): NavBarMenuItem[] {
     return [
@@ -25,21 +38,30 @@ function menu(translate: Function): NavBarMenuItem[] {
 const NavBar: React.FC<{ children: ReactNode }> = props => {
     const { translate } = useTranslations();
     const location = useLocation();
+    const { isMobile } = useSize();
+
     const menuItems = menu(translate);
     const shouldHideNav = routesWithoutNav.some(route => location.pathname.startsWith(route));
 
     if (shouldHideNav) {
         return <>{props.children}</>;
-    } else {
-        return (
-            <div className={css.navAppWrapper}>
-                <nav className={css.topNavBar}>
-                    <NavBarMenuList items={menuItems} currentPath={location.pathname} />
-                </nav>
-                <div>{props.children}</div>
-            </div>
-        );
     }
+    return (
+        <div className={css.navAppWrapper}>
+            <nav className={css.topNavBar}>
+                {isMobile ? (
+                    <MobileNavBar
+                        items={menuItems}
+                        currentPath={location.pathname}
+                        moreText={translate("NAV_BAR.MORE")}
+                    />
+                ) : (
+                    <DesktopNavBar items={menuItems} currentPath={location.pathname} />
+                )}
+            </nav>
+            <div className={css.childrenWrapper}>{props.children}</div>
+        </div>
+    );
 };
 
 export default React.memo(NavBar);
