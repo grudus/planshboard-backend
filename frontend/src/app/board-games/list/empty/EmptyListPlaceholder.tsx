1b56c271402523964a23a3502d0590b4c3caa202
grudus <jakgruda@gmail.com>
2020-03-25 23:30:55 +0100

    Create board game's empty state view

---
+++ frontend/src/app/board-games/list/empty/EmptyListPlaceholder.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import css from "app/board-games/list/empty/empty-list-placeholder.module.scss";
+import { ReactComponent as EmptyImage } from "app/board-games/list/empty.svg";
+import MediumTitle from "library/text/MediumTitle";
+import useTranslations from "app/locale/hooks/useTranslations";
+
+const EmptyListPlaceholder: React.FC = () => {
+    const { translate } = useTranslations();
+    return (
+        <div className={css.emptyWrapper}>
+            {<EmptyImage />}
+            <MediumTitle className={css.emptyText}>
+                {translate("BOARD_GAMES.LIST.EMPTY.TOP")}
+                <br />
+                {translate("BOARD_GAMES.LIST.EMPTY.BOTTOM")}
+            </MediumTitle>
+        </div>
+    );
+};
+
+export default React.memo(EmptyListPlaceholder);
