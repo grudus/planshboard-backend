9d86a0864661d49adea8f451e25d99067ccf7d20
grudus <grudus@users.noreply.github.com>
2020-03-25 23:30:55 +0100

    Create board game's empty state view

--- frontend/src/app/board-games/list/BoardGameList.tsx
+++ frontend/src/app/board-games/list/BoardGameList.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import { useHttpDispatch } from "app/shared/store/httpRequestActions";
 import { getBoardGamesRequest } from "app/board-games/BoardGameApi";
-import { useRedux } from "store/rootReducer";
 import css from "./board-game-list.module.scss";
 import BoardGameListItem from "app/board-games/list-item/BoardGameListItem";
+import EmptyListPlaceholder from "app/board-games/list/empty/EmptyListPlaceholder";
+import { useRedux } from "store/rootReducer";
 
 const BoardGameList: React.FunctionComponent<any> = () => {
     const dispatch = useHttpDispatch();
@@ -13,6 +14,10 @@ const BoardGameList: React.FunctionComponent<any> = () => {
         getBoardGamesRequest(dispatch);
         // eslint-disable-next-line
     }, []);
+
+    if (boardGames?.length === 0) {
+        return <EmptyListPlaceholder />;
+    }
 
     return (
         <div>
