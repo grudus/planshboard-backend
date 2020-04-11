0cee92e4dc635a27fbd4695fc4891bcabe0ff5c2
grudus <grudus@users.noreply.github.com>
2020-04-11 15:54:00 +0200

    Check for name existence before creating new game

--- frontend/src/app/board-games/add/AddBoardGame.tsx
+++ frontend/src/app/board-games/add/AddBoardGame.tsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import BoardGameForm from "app/board-games/form/BoardGameForm";
 import { useHistory } from "react-router-dom";
 import { appRoutes } from "app/routing/routes";
 import { useHttpDispatch } from "app/shared/store/httpRequestActions";
 import { addBoardGameRequest } from "app/board-games/BoardGameApi";
+import useTranslations from "app/locale/hooks/useTranslations";
+import { getErrorCode } from "utils/httpUtils";
 
 const AddBoardGame: React.FC = () => {
     const history = useHistory();
     const dispatch = useHttpDispatch();
+    const { translate } = useTranslations();
+    const [error, setError] = useState("");
 
     const onSubmit = async (name: string) => {
-        await addBoardGameRequest(dispatch, { name });
-        onCancel();
+        try {
+            setError("");
+            await addBoardGameRequest(dispatch, { name });
+            onCancel();
+        } catch (e) {
+            const code = getErrorCode(e);
+            setError(translate(`ERRORS.${code}`));
+        }
     };
     const onCancel = () => {
         history.push(appRoutes.boardGames.list);
     };
-    return <BoardGameForm title="Dodaj grę" onSubmit={onSubmit} onCancel={onCancel} />;
+    return (
+        <BoardGameForm
+            title={translate("BOARD_GAMES.ADD.TITLE")}
+            onSubmit={onSubmit}
+            onCancel={onCancel}
+            error={error}
+        />
+    );
 };
 
 export default AddBoardGame;
