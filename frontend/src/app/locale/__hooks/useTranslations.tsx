9e5cc7ceebfafd431905bd340bfa4630ede327d6
grudus <grudus@users.noreply.github.com>
2020-04-14 21:23:42 +0200

    Memoize and cache translation function

--- frontend/src/app/locale/__hooks/useTranslations.tsx
+++ frontend/src/app/locale/__hooks/useTranslations.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useRedux } from "store/rootReducer";
 import { getNestedTranslation } from "utils/objectUtils";
+import { memoize } from "lodash";
+import { Translations } from "app/locale/__store/localeStore";
+
+const performTranslate = (key: string, translations: Translations): string =>
+    key && getNestedTranslation(key, translations);
+
+const memoizedTranslation = memoize(performTranslate);
 
 const useTranslations = (): { translate: Function } => {
     const { translations } = useRedux(state => state.locale);
 
-    const translate = React.useCallback((key: string): string => key && getNestedTranslation(key, translations), [
-        translations,
-    ]);
-    return { translate };
+    const cachedTranslate = useCallback(key => memoizedTranslation(key, translations), [translations]);
+    return { translate: cachedTranslate };
 };
 
 export default useTranslations;
