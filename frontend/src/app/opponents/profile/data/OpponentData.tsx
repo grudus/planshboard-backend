ca159b0153f32bd65f4af18242a12f41969aebac
grudus <grudus@users.noreply.github.com>
2020-04-19 14:46:43 +0200

    Disable editing opponent linked with current user

--- frontend/src/app/opponents/profile/data/OpponentData.tsx
+++ frontend/src/app/opponents/profile/data/OpponentData.tsx
@@ -3,12 +3,20 @@ import { Opponent } from "app/opponents/__models/OpponentModels";
 import Avatar from "library/avatar/Avatar";
 import Icons from "library/icons/Icons";
 import css from "./opponent-data.module.scss";
+import useTranslations from "app/locale/__hooks/useTranslations";
 
 interface OpponentDataProps {
     opponent?: Opponent;
 }
 
 const OpponentData: React.FC<OpponentDataProps> = props => {
+    const { translate } = useTranslations();
+    const isCurrentUser = props.opponent?.linkedUser?.status === "LINKED_WITH_CREATOR" ?? false;
+
+    const linkedUserInfo = () => {
+        if (!props.opponent) return;
+        return isCurrentUser ? translate("OPPONENTS.PROFILE.LINKED_WITH_CREATOR") : props.opponent.linkedUser?.userName;
+    };
     return (
         <article className={css.wrapper}>
             <Avatar
@@ -20,7 +28,7 @@ const OpponentData: React.FC<OpponentDataProps> = props => {
             />
             <div className={css.namesWrapper}>
                 <h4 className={css.opponentName}>{props.opponent?.name}</h4>
-                <h6 className={css.existingUserName}>{props.opponent?.linkedUser?.userName}</h6>
+                <h6 className={css.existingUserName}>{linkedUserInfo()}</h6>
             </div>
         </article>
     );
