b56cf01e211f59bee5c0999938e0484fb98db57c
grudus <grudus@users.noreply.github.com>
2020-05-05 14:06:43 +0200

    Create searchable dropdown component

--- frontend/src/app/plays/form/play-form-opponents/PlayFormOpponents.tsx
+++ frontend/src/app/plays/form/play-form-opponents/PlayFormOpponents.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Opponent } from "app/opponents/__models/OpponentModels";
 import css from "./play-form-opponents.module.scss";
 import Chip from "library/chip/Chip";
 import FlipMove from "react-flip-move";
+import Dropdown from "library/dropdown/Dropdown";
 
 interface PlayFormOpponentsProps {
     frequentOpponents: Opponent[];
@@ -10,6 +11,27 @@ interface PlayFormOpponentsProps {
 }
 
 const PlayFormOpponents: React.FC<PlayFormOpponentsProps> = props => {
+    const [options, setOptions] = useState([
+        { label: "Mamcia", value: "mama" },
+        { label: "Tatcia", value: "tata" },
+        { label: "Elżunia", value: "elżunia" },
+        { label: "Member One", value: "member" },
+    ]);
+    const [loading, setLoading] = useState(false);
+
+    const onOpponentSelect = (aaa: any) => {
+        console.log("@@@@@@@@@@@@@2", aaa);
+    };
+
+    const onCreateOpponent = (name: string) => {
+        console.log("$###############", name);
+        setLoading(true);
+        setTimeout(() => {
+            setOptions([...options, { label: name, value: name }]);
+            setLoading(false);
+        }, 20_000);
+    };
+
     return (
         <section>
             <h6 className={css.title}>Wybierz graczy</h6>
@@ -19,8 +41,14 @@ const PlayFormOpponents: React.FC<PlayFormOpponentsProps> = props => {
                         <Chip text={op.name} onClick={() => props.onSelect(op)} />
                     </li>
                 ))}
-                <span className={css.more}>Pokaż wszystkich</span>
+                <span className={css.more}>Więcej</span>
             </FlipMove>
+            <Dropdown
+                options={options}
+                onSelect={onOpponentSelect}
+                onCreateOption={onCreateOpponent}
+                isLoading={loading}
+            />
         </section>
     );
 };
---
+++ frontend/src/library/dropdown/Dropdown.tsx
@@ -0,0 +1,84 @@
+import React, { CSSProperties } from "react";
+import CreatableSelect from "react-select/creatable";
+import useTranslations from "../../app/locale/__hooks/useTranslations";
+import useTheme from "../../app/shared/hooks/useTheme";
+import { OptionsType, ValueType } from "react-select/src/types";
+import RingLoading from "library/loading/RingLoading";
+import css from "./dropdown.module.scss";
+import { SelectComponents } from "react-select/src/components";
+
+interface DropdownProps<T> {
+    options: OptionsType<T>;
+    onSelect: (value: ValueType<T>) => void;
+    onCreateOption?: (inputValue: string) => void;
+    createTextKey?: string;
+    selectTextKey?: string;
+    isLoading?: boolean;
+}
+
+const Dropdown: React.FC<DropdownProps<{ label: string; value: string }>> = props => {
+    const { translate } = useTranslations();
+    const theme = useTheme();
+
+    const components: Partial<SelectComponents<any>> = {
+        ClearIndicator: null,
+        LoadingIndicator: a => (
+            <div className={css.loadingWrapper} {...a}>
+                <RingLoading borderWidth={2} size={20} />
+            </div>
+        ),
+    };
+
+    const customStyles = {
+        menuList: (provided: CSSProperties) => ({
+            ...provided,
+            background: theme["--card-background"],
+        }),
+
+        indicatorSeparator: () => ({
+            display: "none",
+        }),
+
+        option: (provided: CSSProperties, state: any) => ({
+            ...provided,
+            color: theme["--input-text"],
+            background: state.isFocused ? theme["--input-background"] : theme["--card-background"],
+
+            "&:hover": {
+                background: theme["--card-background"],
+            },
+            padding: "13px 16px 13px",
+            fontSize: "14px",
+        }),
+        control: (provided: CSSProperties) => ({
+            ...provided,
+            background: theme["--input-background"],
+            border: `1px solid ${theme["--input-border-normal"]}`,
+            boxShadow: "none",
+            fontSize: "14px",
+            "&:hover": {
+                border: `1px solid ${theme["--input-border-normal"]}`,
+            },
+        }),
+        singleValue: (provided: CSSProperties) => ({
+            ...provided,
+            color: theme["--input-text"],
+        }),
+    };
+
+    return (
+        <CreatableSelect
+            options={props.options}
+            onChange={props.onSelect}
+            formatCreateLabel={value => `${translate(props.createTextKey ?? "DROPDOWN.CREATE_LABEL")} "${value}"`}
+            styles={customStyles}
+            placeholder={translate(props.selectTextKey ?? "DROPDOWN.SELECT_LABEL")}
+            onCreateOption={props.onCreateOption}
+            isLoading={props.isLoading}
+            isDisabled={props.isLoading}
+            components={components}
+        />
+    );
+};
+
+export default Dropdown;
