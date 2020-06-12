cc8c33e14a37132fa19edab9ec68bc4bdd565964
grudus <jakgruda@gmail.com>
2020-06-12 22:50:47 +0200

    Add opponent from dropdown when it doesn't exist

--- frontend/src/app/opponents/__store/opponentActions.ts
+++ frontend/src/app/opponents/__store/opponentActions.ts
@@ -1,7 +1,12 @@
 import { createAction } from "@reduxjs/toolkit";
-import { OpponentListItem, OpponentWithStats, SaveOpponentRequest } from "app/opponents/__models/OpponentModels";
+import {
+    Opponent,
+    OpponentListItem,
+    OpponentWithStats,
+    SaveOpponentRequest,
+} from "app/opponents/__models/OpponentModels";
 
 export const getAllOpponentsSuccessAction = createAction<OpponentListItem[]>("GET_ALL_OPPONENTS_SUCCESS");
 export const getSingleOpponentSuccessAction = createAction<OpponentWithStats>("GET_SINGLE_OPPONENT_SUCCESS");
-export const createOpponentSuccessAction = createAction<OpponentWithStats>("CREATE_OPPONENT_SUCCESS");
+export const createOpponentSuccessAction = createAction<Opponent>("CREATE_OPPONENT_SUCCESS");
 export const updateOpponentSuccessAction = createAction<SaveOpponentRequest>("UPDATE_OPPONENT_SUCCESS");
--- frontend/src/app/opponents/dropdown/OpponentsDropdown.tsx
+++ frontend/src/app/opponents/dropdown/OpponentsDropdown.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useRedux } from "store/rootReducer";
 import Dropdown, { DropdownProps } from "library/dropdown/Dropdown";
+import { useHttpDispatch } from "app/shared/store/httpRequestActions";
+import { createOpponentRequest } from "app/opponents/OpponentApi";
+import { ValueType } from "react-select";
 
 interface OpponentsDropdownProps extends Partial<DropdownProps<any>> {
     alreadyUsedOpponents?: Set<number>;
@@ -8,16 +11,23 @@ interface OpponentsDropdownProps extends Partial<DropdownProps<any>> {
 
 const OpponentsDropdown: React.FC<OpponentsDropdownProps> = props => {
     const opponents = useRedux(state => state.opponent.list);
+    const dispatch = useHttpDispatch();
+
+    const createOpponent = async (opponentName: string) => {
+        const response = await createOpponentRequest(dispatch, { opponentName });
+        const { id } = response;
+        props.onSelect?.({ id, name: opponentName });
+    };
+
+    const onSelect = (a: ValueType<any>) => {
+        props.onSelect?.(a.value);
+    };
 
     const options = opponents
         .filter(op => !props.alreadyUsedOpponents?.has(op.id))
         .map(op => ({ label: op.name, value: op }));
 
-    const onSelect = (a: any) => {
-        props.onSelect?.(a.value);
-    };
-
-    return <Dropdown options={options} {...props} onSelect={onSelect} />;
+    return <Dropdown options={options} {...props} onSelect={onSelect} onCreateOption={createOpponent} />;
 };
 
 export default OpponentsDropdown;
