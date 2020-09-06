674432be075c3214e65ea1c15b93111d14d4f9b0
grudus <grudus@users.noreply.github.com>
2020-09-06 13:50:55 +0200

    Create radio buttons with game type

--- frontend/src/app/board-games/form/board-game-options/BoardGameOptions.tsx
+++ frontend/src/app/board-games/form/board-game-options/BoardGameOptions.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import FancyRadio from "library/fancy-radio/FancyRadio";
+import Icons from "library/icons/Icons";
+import css from "./board-game-options.module.scss";
+import { FinalResult } from "app/plays/__models/PlayModels";
 
 const BoardGameOptions: React.FC = () => {
+    const [gameType, setGameType] = useState("REGULAR");
+
+    const changeGameType = (a: ChangeEvent<HTMLInputElement>) => {
+        const result = a.target.value as FinalResult;
+        setGameType(result);
+    };
+
     return (
         <div>
-            <h6>Dodatkowe dane o rozgrywce</h6>
-            <span>Rodzaj gry:</span>
-            <span>Kooperacyjna</span>
-            <span>Tradycyjna</span>
+            <h6>Rodzaj gry</h6>
+            <div className={css.gameTypeWrapper} onChange={changeGameType}>
+                <FancyRadio
+                    text={"Tradycyjna"}
+                    icon={Icons.HiveIcon}
+                    value="REGULAR"
+                    inputName="type"
+                    selectedValue={gameType}
+                    selectedClassName={css.selectedType}
+                />
+                <FancyRadio
+                    text={"Kooperacyjna"}
+                    icon={Icons.PolygonIcon}
+                    value="COOPERATIVE"
+                    inputName="type"
+                    selectedValue={gameType}
+                    selectedClassName={css.selectedType}
+                />
+            </div>
         </div>
     );
 };
