e12c533e540e3fb34e6f40553bc0114029bcb647
grudus <grudus@users.noreply.github.com>
2020-12-02 22:28:05 +0100

    Create mockup placeholder when no board games

--- frontend/src/app/board-games/__store/boardGameReducer.ts
+++ frontend/src/app/board-games/__store/boardGameReducer.ts
@@ -12,6 +12,7 @@ import { AddBoardGameRequest, DeleteBoardGameRequest } from "app/board-games/Boa
 export interface BoardGameStore {
     list: BoardGame[];
     single?: SingleBoardGame;
+    boardGameExists?: boolean;
 }
 
 const initialState: BoardGameStore = {
@@ -22,10 +23,12 @@ export const boardGamesReducer = createReducer<BoardGameStore>(initialState, {
     [getBoardGamesSuccessAction.type]: (state, action: PayloadAction<BoardGame[]>) => ({
         ...state,
         list: action.payload,
+        boardGameExists: !!action.payload.length,
     }),
     [addBoardGameSuccessAction.type]: (state, action: PayloadAction<{ id: number; request: AddBoardGameRequest }>) => {
         const { id, request } = action.payload;
-        return { ...state, list: [...state.list, { id, name: request.name, createdAt: new Date() }] };
+        const list = [...state.list, { id, name: request.name, createdAt: new Date() }];
+        return { ...state, list, boardGameExists: true };
     },
     [getSingleBoardGameSuccessAction.type]: (state, action: PayloadAction<SingleBoardGame>) => ({
         ...state,
@@ -35,8 +38,12 @@ export const boardGamesReducer = createReducer<BoardGameStore>(initialState, {
         ...state,
         list: state.list.map(game => (game.id === action.payload.id ? { ...game, name: action.payload.name } : game)),
     }),
-    [deleteBoardGameSuccessAction.type]: (state, action: PayloadAction<DeleteBoardGameRequest>) => ({
-        ...state,
-        list: state.list.filter(game => game.id !== action.payload.id),
-    }),
+    [deleteBoardGameSuccessAction.type]: (state, action: PayloadAction<DeleteBoardGameRequest>) => {
+        const list = state.list.filter(game => game.id !== action.payload.id);
+        return {
+            ...state,
+            list: list,
+            boardGameExists: !!list.length,
+        };
+    },
 });
