a57647bc4d2fcd5cdbbfc6f53fff817c81e199d9
grudus <grudus@users.noreply.github.com>
2020-12-04 14:43:05 +0100

    Create mockup placeholder when no plays

--- frontend/src/app/plays/list/add-button/AddPlayButton.tsx
+++ frontend/src/app/plays/list/add-button/AddPlayButton.tsx
@@ -4,23 +4,23 @@ import LinkButton from "library/link-button/LinkButton";
 import { appRoutes } from "app/routing/routes";
 import Icons from "library/icons/Icons";
 
-interface AddOpponentButtonProps {
-    className: string;
+interface AddPlayButtonProps {
+    className?: string;
 }
 
-const AddOpponentButton: React.FC<AddOpponentButtonProps> = props => {
+const AddPlayButton: React.FC<AddPlayButtonProps> = props => {
     const { translate } = useTranslations();
 
     return (
         <LinkButton
-            href={appRoutes.opponents.add}
+            href={appRoutes.plays.add}
             tabIndex={1}
             leftIcon={Icons.PlusIcon}
-            text={translate("OPPONENTS.LIST.ADD_BUTTON")}
+            text={translate("PLAYS.LIST.ADD_BUTTON")}
             color="accent"
             {...props}
         />
     );
 };
 
-export default React.memo(AddOpponentButton);
+export default React.memo(AddPlayButton);
---
+++ frontend/src/app/plays/list/no-board-games/NoBoardGamesPlaceholder.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import useTranslations from "app/locale/__hooks/useTranslations";
+import css from "./no-board-games-placeholder.module.scss";
+import { ReactComponent as EmptyImage } from "./no-board-games.svg";
+import MediumTitle from "library/text/MediumTitle";
+
+const NoBoardGamesPlaceholder: React.FC = () => {
+    const { translate } = useTranslations();
+
+    return (
+        <div className={css.emptyWrapper}>
+            {<EmptyImage />}
+            <MediumTitle className={css.emptyText}>
+                {translate("BOARD_GAMES.LIST.EMPTY.TOP")}
+                <br />
+                {translate("BOARD_GAMES.LIST.EMPTY.BOTTOM")}
+            </MediumTitle>
+        </div>
+    );
+};
+
+export default NoBoardGamesPlaceholder;
