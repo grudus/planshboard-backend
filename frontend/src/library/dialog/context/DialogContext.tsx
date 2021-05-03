8d83512841f06a29fd759ab0df805b75d33955bd
grudus <grudus@users.noreply.github.com>
2021-05-03 11:12:35 +0200

    Fix dialog animation; Refactor dialog context provider

--- frontend/src/library/dialog/context/DialogContext.tsx
+++ frontend/src/library/dialog/context/DialogContext.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { DialogProps } from "library/dialog/Dialog";
+import css from "../dialog.module.scss";
 
 type DialogElem = React.ReactElement<DialogProps>;
 
@@ -15,18 +16,26 @@ export const DialogProvider: React.FC = props => {
     const [dialogs, setDialogs] = useState<{ id: number; dialog: DialogElem }[]>([]);
     const [maxId, setMaxId] = useState(0);
 
+    const removeDialogAfterAnimationEnds = (idToClose: number) => {
+        setTimeout(() => {
+            setDialogs(_dialogs => _dialogs.filter(({ id }) => id !== idToClose));
+        }, parseInt(css.hideAnimationTime, 10));
+    };
+
+    const startHidingDialogAnimation = (dialog: DialogElem, newId: number) => {
+        const dialogToClose = React.cloneElement(dialog, { open: false, key: newId });
+        setDialogs(_dialogs => _dialogs.map(dd => (dd.id === newId ? { id: newId, dialog: dialogToClose } : dd)));
+    };
+
     const showDialog = (dialog: DialogElem) => {
         const newId = maxId + 1;
 
         const onClose = () => {
-            const dialogToClose = React.cloneElement(dialog, { open: false, key: newId });
-            setDialogs(_dialogs => _dialogs.map(dd => (dd.id === newId ? { id: newId, dialog: dialogToClose } : dd)));
-            setTimeout(() => {
-                setDialogs(_dialogs => _dialogs.filter(({ id }) => id !== newId));
-            }, 125);
+            startHidingDialogAnimation(dialog, newId);
+            removeDialogAfterAnimationEnds(newId);
         };
 
-        const boundDialog = React.cloneElement(dialog, { onCancel: onClose, key: newId });
+        const boundDialog = React.cloneElement(dialog, { onCancel: onClose, open: true, key: newId });
 
         setDialogs([...dialogs, { id: newId, dialog: boundDialog }]);
         setMaxId(newId);
@@ -34,7 +43,7 @@ export const DialogProvider: React.FC = props => {
 
     return (
         <DialogContext.Provider value={{ showDialog }}>
-            {dialogs.map(({ dialog, id }) => dialog)}
+            {dialogs.map(({ dialog }) => dialog)}
             {props.children}
         </DialogContext.Provider>
     );
