b4ed0ab1f23253e737924269e949d5caafb9917e
grudus <grudus@users.noreply.github.com>
2021-05-26 11:35:07 +0200

    Use redux thunk for play actions

--- frontend/src/app/plays/__store/playActions.ts
+++ frontend/src/app/plays/__store/playActions.ts
@@ -1,7 +1,18 @@
-import { createAction } from "@reduxjs/toolkit";
 import { TagCounts } from "app/plays/__models/TagModels";
 import { PlayListItem } from "app/plays/__models/PlayModels";
+import { baseHttpAction } from "app/shared/store/httpRequestActions";
+import PlayApi from "app/plays/PlayApi";
 
-export const getTagsSuccessAction = createAction<TagCounts[]>("GET_TAGS_WITH_COUNT");
-export const savePlaySuccessAction = createAction("SAVE_PLAY_SUCCESS");
-export const getAllPlaysAction = createAction<PlayListItem[]>("GET_ALL_PLAYS_SUCCESS");
+const getTags = baseHttpAction<TagCounts[], void>("GET_TAGS_WITH_COUNT", PlayApi.getTags);
+
+const savePlay = baseHttpAction("SAVE_PLAY", PlayApi.createPlay);
+
+const getAllPlays = baseHttpAction<PlayListItem[], void>("GET_ALL_PLAYS", PlayApi.getAllPlays);
+
+const PlayActions = {
+    getTags,
+    savePlay,
+    getAllPlays,
+};
+
+export default PlayActions;
